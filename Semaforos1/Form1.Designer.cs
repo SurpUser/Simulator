5860a146ece1e2af42fbbaf0e5073dd4ea2e22d9
SurpUser <francisjcv@hotmail.com>
2016-07-27 15:44:24 -0400

    Subiendo Proyecto

---
+++ Semaforos1/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Semaforos1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.Verde3OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Amarillo3OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Rojo3OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Verde1OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Amarillo1OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Rojo1OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Verde2DOvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Verde2OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Amarillo2OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.Rojo2OvalShape = new Microsoft.VisualBasic.PowerPacks.OvalShape();
+            this.IniciarButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shapeContainer1
+            // 
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.Verde3OvalShape,
+            this.Amarillo3OvalShape,
+            this.Rojo3OvalShape,
+            this.Verde1OvalShape,
+            this.Amarillo1OvalShape,
+            this.Rojo1OvalShape,
+            this.Verde2DOvalShape,
+            this.Verde2OvalShape,
+            this.Amarillo2OvalShape,
+            this.Rojo2OvalShape});
+            this.shapeContainer1.Size = new System.Drawing.Size(792, 753);
+            this.shapeContainer1.TabIndex = 0;
+            this.shapeContainer1.TabStop = false;
+            // 
+            // Verde3OvalShape
+            // 
+            this.Verde3OvalShape.BackColor = System.Drawing.Color.Lime;
+            this.Verde3OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Verde3OvalShape.BorderColor = System.Drawing.Color.Lime;
+            this.Verde3OvalShape.Location = new System.Drawing.Point(497, 505);
+            this.Verde3OvalShape.Name = "Verde3OvalShape";
+            this.Verde3OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Verde3OvalShape.Click += new System.EventHandler(this.Verde3OvalShape_Click);
+            // 
+            // Amarillo3OvalShape
+            // 
+            this.Amarillo3OvalShape.BackColor = System.Drawing.Color.Gold;
+            this.Amarillo3OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Amarillo3OvalShape.BorderColor = System.Drawing.Color.Gold;
+            this.Amarillo3OvalShape.Location = new System.Drawing.Point(496, 479);
+            this.Amarillo3OvalShape.Name = "Amarillo3OvalShape";
+            this.Amarillo3OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Amarillo3OvalShape.Click += new System.EventHandler(this.Amarillo3OvalShape_Click);
+            // 
+            // Rojo3OvalShape
+            // 
+            this.Rojo3OvalShape.BackColor = System.Drawing.Color.Red;
+            this.Rojo3OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Rojo3OvalShape.BorderColor = System.Drawing.Color.Red;
+            this.Rojo3OvalShape.Location = new System.Drawing.Point(496, 450);
+            this.Rojo3OvalShape.Name = "Rojo3OvalShape";
+            this.Rojo3OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Rojo3OvalShape.Click += new System.EventHandler(this.Rojo3OvalShape_Click);
+            // 
+            // Verde1OvalShape
+            // 
+            this.Verde1OvalShape.BackColor = System.Drawing.Color.Lime;
+            this.Verde1OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Verde1OvalShape.BorderColor = System.Drawing.Color.Lime;
+            this.Verde1OvalShape.Location = new System.Drawing.Point(289, 293);
+            this.Verde1OvalShape.Name = "Verde1OvalShape";
+            this.Verde1OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Verde1OvalShape.Click += new System.EventHandler(this.Verde1OvalShape_Click);
+            // 
+            // Amarillo1OvalShape
+            // 
+            this.Amarillo1OvalShape.BackColor = System.Drawing.Color.Gold;
+            this.Amarillo1OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Amarillo1OvalShape.BorderColor = System.Drawing.Color.Gold;
+            this.Amarillo1OvalShape.Location = new System.Drawing.Point(257, 293);
+            this.Amarillo1OvalShape.Name = "Amarillo1OvalShape";
+            this.Amarillo1OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Amarillo1OvalShape.Click += new System.EventHandler(this.Amarillo1OvalShape_Click);
+            // 
+            // Rojo1OvalShape
+            // 
+            this.Rojo1OvalShape.BackColor = System.Drawing.Color.Red;
+            this.Rojo1OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Rojo1OvalShape.BorderColor = System.Drawing.Color.Red;
+            this.Rojo1OvalShape.FillColor = System.Drawing.Color.Red;
+            this.Rojo1OvalShape.FillGradientColor = System.Drawing.Color.Red;
+            this.Rojo1OvalShape.Location = new System.Drawing.Point(225, 294);
+            this.Rojo1OvalShape.Name = "Rojo1OvalShape";
+            this.Rojo1OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Rojo1OvalShape.Click += new System.EventHandler(this.Rojo1OvalShape_Click);
+            // 
+            // Verde2DOvalShape
+            // 
+            this.Verde2DOvalShape.BackColor = System.Drawing.Color.Lime;
+            this.Verde2DOvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Verde2DOvalShape.BorderColor = System.Drawing.Color.Lime;
+            this.Verde2DOvalShape.Location = new System.Drawing.Point(537, 165);
+            this.Verde2DOvalShape.Name = "Verde2DOvalShape";
+            this.Verde2DOvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Verde2DOvalShape.Click += new System.EventHandler(this.Verde2DOvalShape_Click);
+            // 
+            // Verde2OvalShape
+            // 
+            this.Verde2OvalShape.BackColor = System.Drawing.Color.Lime;
+            this.Verde2OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Verde2OvalShape.BorderColor = System.Drawing.Color.Lime;
+            this.Verde2OvalShape.Location = new System.Drawing.Point(508, 164);
+            this.Verde2OvalShape.Name = "Verde2OvalShape";
+            this.Verde2OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Verde2OvalShape.Click += new System.EventHandler(this.Verde2OvalShape_Click);
+            // 
+            // Amarillo2OvalShape
+            // 
+            this.Amarillo2OvalShape.BackColor = System.Drawing.Color.Gold;
+            this.Amarillo2OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Amarillo2OvalShape.BorderColor = System.Drawing.Color.Gold;
+            this.Amarillo2OvalShape.Location = new System.Drawing.Point(480, 164);
+            this.Amarillo2OvalShape.Name = "Amarillo2OvalShape";
+            this.Amarillo2OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Amarillo2OvalShape.Click += new System.EventHandler(this.Amarillo2OvalShape_Click);
+            // 
+            // Rojo2OvalShape
+            // 
+            this.Rojo2OvalShape.BackColor = System.Drawing.Color.Red;
+            this.Rojo2OvalShape.BackStyle = Microsoft.VisualBasic.PowerPacks.BackStyle.Opaque;
+            this.Rojo2OvalShape.BorderColor = System.Drawing.Color.Red;
+            this.Rojo2OvalShape.Location = new System.Drawing.Point(452, 163);
+            this.Rojo2OvalShape.Name = "Rojo2OvalShape";
+            this.Rojo2OvalShape.Size = new System.Drawing.Size(20, 19);
+            this.Rojo2OvalShape.Click += new System.EventHandler(this.Rojo2OvalShape_Click);
+            // 
+            // IniciarButton
+            // 
+            this.IniciarButton.Location = new System.Drawing.Point(53, 106);
+            this.IniciarButton.Name = "IniciarButton";
+            this.IniciarButton.Size = new System.Drawing.Size(75, 23);
+            this.IniciarButton.TabIndex = 1;
+            this.IniciarButton.Text = "Iniciar";
+            this.IniciarButton.UseVisualStyleBackColor = true;
+            this.IniciarButton.Click += new System.EventHandler(this.IniciarButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::Semaforos1.Properties.Resources.ProyectoCarretera;
+            this.ClientSize = new System.Drawing.Size(792, 753);
+            this.Controls.Add(this.IniciarButton);
+            this.Controls.Add(this.shapeContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Verde3OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Amarillo3OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Rojo3OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Verde1OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Amarillo1OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Rojo1OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Verde2DOvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Verde2OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Amarillo2OvalShape;
+        private Microsoft.VisualBasic.PowerPacks.OvalShape Rojo2OvalShape;
+        private System.Windows.Forms.Button IniciarButton;
+    }
+}
+
---
+++ Semaforos1/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Semaforos1
+{
+    public partial class Form1 : Form
+    {
+        int x;
+        public Form1()
+        {
+            InitializeComponent();
+            x = 0;
+
+            Rojo1OvalShape.BackColor = Color.Red;
+            Rojo2OvalShape.BackColor = Color.Red;
+            Rojo3OvalShape.BackColor = Color.Red;
+
+            Amarillo1OvalShape.BackColor = Color.Gray;
+            Amarillo2OvalShape.BackColor = Color.Gray;
+            Amarillo3OvalShape.BackColor = Color.Gray;
+
+            Verde1OvalShape.BackColor = Color.Gray;
+            Verde2OvalShape.BackColor = Color.Gray;
+            Verde3OvalShape.BackColor = Color.Gray;
+            Verde2DOvalShape.BackColor = Color.Gray;
+
+        }
+
+        private void IniciarButton_Click(object sender, EventArgs e)
+        {
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                
+                if (x == 1)
+                {
+                    Verde1OvalShape.BackColor = Color.Lime;
+                    Rojo1OvalShape.BackColor = Color.Gray;
+                    Amarillo1OvalShape.BackColor = Color.Gray;
+                    Rojo2OvalShape.BackColor = Color.Gray;
+                    Verde2OvalShape.BackColor = Color.Lime;
+                    Verde2DOvalShape.BackColor = Color.Gray;
+                    Rojo3OvalShape.BackColor = Color.Red;
+                    Verde3OvalShape.BackColor = Color.Gray;
+                    Amarillo3OvalShape.BackColor = Color.Gray;
+                    Thread.Sleep(1000);
+                }
+
+                if (x == 2)
+                {
+                    Verde1OvalShape.BackColor = Color.Gray;
+                    Rojo1OvalShape.BackColor = Color.Gray;
+                    Amarillo1OvalShape.BackColor = Color.Gold;
+                    Rojo1OvalShape.BackColor = Color.Gray;
+                    Verde2OvalShape.BackColor = Color.Lime;
+                    Verde2DOvalShape.BackColor = Color.Gray;
+                    Rojo3OvalShape.BackColor = Color.Red;
+                    Verde3OvalShape.BackColor = Color.Gray;
+                    
+                }
+
+                if (x == 3)
+                {
+                    Verde1OvalShape.BackColor = Color.Gray;
+                    Amarillo1OvalShape.BackColor = Color.Gray;
+                    Rojo1OvalShape.BackColor = Color.Red;
+                    Rojo3OvalShape.BackColor = Color.Red;
+                    Verde2DOvalShape.BackColor = Color.Gray;
+                    Verde2OvalShape.BackColor = Color.Gray;
+                    Rojo2OvalShape.BackColor = Color.Gray;
+                    Amarillo2OvalShape.BackColor = Color.Gold;
+                    
+                }
+
+
+                if (x == 4)
+                {
+                    Rojo2OvalShape.BackColor = Color.Red;
+                    Amarillo2OvalShape.BackColor = Color.Gray;
+                    Verde2DOvalShape.BackColor = Color.Lime;
+
+
+                }
+
+
+                if (x == 5)
+                {
+                    Verde1OvalShape.BackColor = Color.Gray;
+                    Amarillo1OvalShape.BackColor = Color.Gray;
+                    Rojo1OvalShape.BackColor = Color.Red;
+                    Rojo3OvalShape.BackColor = Color.Gray;
+                    Verde2DOvalShape.BackColor = Color.Gray;
+                    Verde2OvalShape.BackColor = Color.Gray;
+                    Rojo2OvalShape.BackColor = Color.Red;
+                    Verde3OvalShape.BackColor = Color.Lime;
+
+                }
+
+
+                if (x == 6)
+                {
+                    Amarillo3OvalShape.BackColor = Color.Gold;
+                    Verde3OvalShape.BackColor = Color.Gray;
+                    x = 0;
+
+                }
+
+
+                x++;
+                
+            }
+
+        }
+
+        private void Rojo1OvalShape_Click(object sender, EventArgs e)
+        {
+            Rojo1OvalShape.BackColor = Color.Red;
+        }
+
+        private void Rojo2OvalShape_Click(object sender, EventArgs e)
+        {
+            Rojo2OvalShape.BackColor = Color.Red;
+        }
+
+        private void Rojo3OvalShape_Click(object sender, EventArgs e)
+        {
+            Rojo3OvalShape.BackColor = Color.Red;
+        }
+
+        private void Amarillo1OvalShape_Click(object sender, EventArgs e)
+        {
+            Amarillo1OvalShape.BackColor = Color.Gold;
+        }
+
+        private void Amarillo2OvalShape_Click(object sender, EventArgs e)
+        {
+            Amarillo2OvalShape.BackColor = Color.Gold;
+        }
+
+        private void Amarillo3OvalShape_Click(object sender, EventArgs e)
+        {
+            Amarillo3OvalShape.BackColor = Color.Gold;
+        }
+
+        private void Verde1OvalShape_Click(object sender, EventArgs e)
+        {
+            Verde1OvalShape.BackColor = Color.Gray;
+        }
+
+        private void Verde2OvalShape_Click(object sender, EventArgs e)
+        {
+            Verde2OvalShape.BackColor = Color.Gray;
+        }
+
+        private void Verde2DOvalShape_Click(object sender, EventArgs e)
+        {
+            Verde2DOvalShape.BackColor = Color.Gray;
+        }
+
+        private void Verde3OvalShape_Click(object sender, EventArgs e)
+        {
+            Verde3OvalShape.BackColor = Color.Gray;
+        }
+    }
+}
